27459467eb29dd82b61b404061dc87eadbbbfc47
Robert Dakey <Robert.Dakey@azubiafrica.org>
2021-03-01 14:06:56 +0000

    dashboard meals update

--- pages/Admin/meals/index.js
+++ pages/Admin/meals/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Meals from '../../../components/Admin-dashboard/meals';
+import Meals from '../../../components/Admin-dashboard/meals/meals';
 
 export default function Meals_dashboard() {
   return (
