1b808dfd4b0c5a24c37a4dad2f7d8fc4c9c410a4
Robert Dakey <Robert.Dakey@azubiafrica.org>
2021-03-03 17:02:22 +0000

    endpoints

--- config.js
+++ config.js
@@ -2,5 +2,8 @@ const API_BASE_URL = "http://localhost"
 const API_PORT = 7000
 const Login_endpoint = 'users'
 const AddStudent_endpoint = 'students'
+const Meal_endpoint = 'meals'
+const Menu_endpoint = 'menu'
+const Meal_Order_endpoint = 'orders'
 
-export {API_BASE_URL, API_PORT, Login_endpoint, AddStudent_endpoint,}
+export {API_BASE_URL, API_PORT, Login_endpoint, AddStudent_endpoint, Meal_endpoint, Meal_Order_endpoint}
